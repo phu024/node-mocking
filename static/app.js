0053b1b15a17cb280dea4ae760dea8c0fa430850
Phuwadon Decharam <phuwadon.dec@gmail.com>
2025-06-14 16:29:57 +0700

    feat: refactor product routes and enhance data handling with mockDataInsert

--- static/app.js
+++ static/app.js
@@ -1,11 +1,15 @@
 const API = 'http://localhost:3000'
 
-const populateProducts = async (category) => {
+
+const populateProducts = async (category, method = 'GET', payload) => {
     const products = document.querySelector('#products')
     products.innerHTML = ''
-    const res = await fetch(`${API}/${category}`)
+    const send = method === 'GET' ? {} : {
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(payload)
+    }
+    const res = await fetch(`${API}/${category}`, { method, ...send })
     const data = await res.json()
-
     for (const product of data) {
         const item = document.createElement('product-item')
         for (const key of ['name', 'rrp', 'info']) {
@@ -19,15 +23,29 @@ const populateProducts = async (category) => {
 }
 
 const category = document.querySelector('#category')
+const add = document.querySelector('#add')
 
 category.addEventListener('input', async ({ target }) => {
+    add.style.display = 'block'
     await populateProducts(target.value)
+})
+
+add.addEventListener('submit', async (e) => {
+    e.preventDefault()
+    const { target } = e
+    const payload = {
+        name: target.name.value,
+        rrp: target.rrp.value,
+        info: target.info.value
+    }
+    await populateProducts(category.value, 'POST', payload)
+    target.reset()
 })
 
 customElements.define('product-item', class Item extends HTMLElement {
     constructor() {
         super()
-        const itemTempl = document.querySelector('#item').content
-        this.attachShadow({ mode: 'open' }).appendChild(itemTempl.cloneNode(true))
+        const itemTmpl = document.querySelector('#item').content
+        this.attachShadow({ mode: 'open' }).appendChild(itemTmpl.cloneNode(true))
     }
 })
