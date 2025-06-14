e65f37fca2de31e345cee38c7f768165de96de2e
Phuwadon Decharam <phuwadon.dec@gmail.com>
2025-06-14 16:07:42 +0700

    feat: initialize mock-srv project with Fastify setup
    - Added package.json with dependencies for Fastify and plugins.
    - Created plugins directory with README and sensible support plugin.
    - Established routes directory with example routes for confectionery, electronics, and root.
    - Implemented helper functions for testing and added test cases for routes and plugins.

--- static/app.js
+++ static/app.js
@@ -1,11 +1,11 @@
 const API = 'http://localhost:3000'
 
-
-const populateProducts = async () => {
+const populateProducts = async (category) => {
     const products = document.querySelector('#products')
     products.innerHTML = ''
-    const res = await fetch(API)
+    const res = await fetch(`${API}/${category}`)
     const data = await res.json()
+
     for (const product of data) {
         const item = document.createElement('product-item')
         for (const key of ['name', 'rrp', 'info']) {
@@ -18,17 +18,16 @@ const populateProducts = async () => {
     }
 }
 
-document.querySelector('#fetch').addEventListener('click', async () => {
-    await populateProducts()
+const category = document.querySelector('#category')
+
+category.addEventListener('input', async ({ target }) => {
+    await populateProducts(target.value)
 })
-
-
 
 customElements.define('product-item', class Item extends HTMLElement {
     constructor() {
         super()
-        const itemTmpl = document.querySelector('#item').content
-        this.attachShadow({ mode: 'open' }).appendChild(itemTmpl.cloneNode(true))
+        const itemTempl = document.querySelector('#item').content
+        this.attachShadow({ mode: 'open' }).appendChild(itemTempl.cloneNode(true))
     }
 })
-
